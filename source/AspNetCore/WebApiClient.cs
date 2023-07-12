749504b2fa3e6921f7e363332ae88ec747eb7bdf
Virgilio A. Fornazin <virgiliofornazin@gmail.com>
2023-07-12 00:16:24 -0300

    Http Config Methods

--- source/AspNetCore/WebApiClient.cs
+++ source/AspNetCore/WebApiClient.cs
@@ -64,11 +64,50 @@ public class WebApiClient
         return httpResponse;
     }
 
+    protected async Task<RestResponse> ExecuteHTTPMethodAsyncWith(string httpApiPath, Method requestMethod,
+        Action<RestRequest> configureHttpRequestDelegate)
+    {
+        using var httpClient = CreateRestClient();
+
+        var httpRequest = new RestRequest(httpApiPath)
+        {
+            Method = requestMethod
+        };
+
+        if (configureHttpRequestDelegate is not null)
+        {
+            configureHttpRequestDelegate(httpRequest);
+        }
+
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+        var httpResponse = await httpClient.ExecuteAsync(httpRequest);
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+
+        return httpResponse;
+    }
+
     protected async Task<(RestResponse, TOut?)> ExecuteHTTPMethodWithJsonResponseAsync<TOut>(string httpApiPath, Method requestMethod)
         where TOut : class, new()
     {
 #pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
         var httpResponse = await ExecuteHTTPMethodAsync(httpApiPath, requestMethod);
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+
+        if ((httpResponse.StatusCode != HttpStatusCode.OK) || (httpResponse.Content == null))
+        {
+            return (httpResponse, null);
+        }
+
+        var jsonResponse = JsonSerializer.Deserialize<TOut>(httpResponse.Content, JsonSerializerOptionsExtensionMethods.WebApiOptions);
+
+        return (httpResponse, jsonResponse);
+    }
+
+    protected async Task<(RestResponse, TOut?)> ExecuteHTTPMethodWithJsonResponseAsyncWith<TOut>(string httpApiPath, Method requestMethod, Action<RestRequest> configureHttpRequestDelegate)
+        where TOut : class, new()
+    {
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+        var httpResponse = await ExecuteHTTPMethodAsyncWith(httpApiPath, requestMethod, configureHttpRequestDelegate);
 #pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
 
         if ((httpResponse.StatusCode != HttpStatusCode.OK) || (httpResponse.Content == null))
@@ -99,6 +138,30 @@ public class WebApiClient
         return httpResponse;
     }
 
+    protected async Task<RestResponse> ExecuteHTTPMethodWithBodyAsyncWith(string httpApiPath, Method requestMethod, DataFormat requestFormat, object requestBody,
+        Action<RestRequest> configureHttpRequestDelegate)
+    {
+        using var httpClient = CreateRestClient();
+
+        var httpRequest = new RestRequest(httpApiPath)
+        {
+            Method = requestMethod,
+            RequestFormat = requestFormat
+        }
+        .AddBody(requestBody);
+
+        if (configureHttpRequestDelegate is not null)
+        {
+            configureHttpRequestDelegate(httpRequest);
+        }
+
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+        var httpResponse = await httpClient.ExecuteAsync(httpRequest);
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+
+        return httpResponse;
+    }
+
     protected async Task<RestResponse> ExecuteHTTPMethodWithJsonBodyAsync<TIn>(string httpApiPath, Method requestMethod,
         TIn jsonRequest) where TIn : class
     {
@@ -118,11 +181,52 @@ public class WebApiClient
         return httpResponse;
     }
 
+    protected async Task<RestResponse> ExecuteHTTPMethodWithJsonBodyAsyncWith<TIn>(string httpApiPath, Method requestMethod,
+        TIn jsonRequest, Action<RestRequest> configureHttpRequestDelegate) where TIn : class
+    {
+        using var httpClient = CreateRestClient();
+
+        var httpRequest = new RestRequest(httpApiPath)
+        {
+            Method = requestMethod,
+            RequestFormat = DataFormat.Json
+        }
+        .AddJsonBody(jsonRequest);
+
+        if (configureHttpRequestDelegate is not null)
+        {
+            configureHttpRequestDelegate(httpRequest);
+        }
+
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+        var httpResponse = await httpClient.ExecuteAsync(httpRequest);
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+
+        return httpResponse;
+    }
+
     protected async Task<(RestResponse, TOut?)> ExecuteHTTPMethodWithJsonBodyAndResponseAsync<TIn, TOut>(string httpApiPath, Method requestMethod,
         TIn jsonRequest) where TIn : class where TOut : class, new()
     {
 #pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
         var httpResponse = await ExecuteHTTPMethodWithJsonBodyAsync<TIn>(httpApiPath, requestMethod, jsonRequest);
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
+
+        if ((httpResponse.StatusCode != HttpStatusCode.OK) || (httpResponse.Content == null))
+        {
+            return (httpResponse, null);
+        }
+
+        var jsonResponse = JsonSerializer.Deserialize<TOut>(httpResponse.Content, JsonSerializerOptionsExtensionMethods.WebApiOptions);
+
+        return (httpResponse, jsonResponse);
+    }
+
+    protected async Task<(RestResponse, TOut?)> ExecuteHTTPMethodWithJsonBodyAndResponseAsync<TIn, TOut>(string httpApiPath, Method requestMethod,
+        TIn jsonRequest, Action<RestRequest> configureHttpRequestDelegate) where TIn : class where TOut : class, new()
+    {
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+        var httpResponse = await ExecuteHTTPMethodWithJsonBodyAsyncWith<TIn>(httpApiPath, requestMethod, jsonRequest, configureHttpRequestDelegate);
 #pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
 
         if ((httpResponse.StatusCode != HttpStatusCode.OK) || (httpResponse.Content == null))
